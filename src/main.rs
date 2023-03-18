4444b94f56ad31a5a6b59c7031667b607482b0f0
feuvpi <fredvpg@gmail.com>
2023-03-18 20:40:37 -0300

    adding comments

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,8 @@
-use firebase_rs::*;
-use serde::{Deserialize, Serialize};
-use std::collections::HashMap;
+use firebase_rs::*; // importing firebase_rs library
+use serde::{Deserialize, Serialize}; // importing serde serialization and deserialization traits
+use std::collections::HashMap; // importing HashMap for storing data
 
+// -- define User struct with name, age, and email fields
 #[derive(Serialize, Deserialize, Debug)]
 struct User {
     name: String,
@@ -9,11 +10,13 @@ struct User {
     email: String,
 }
 
+// -- define Response struct with name field
 #[derive(Serialize, Deserialize, Debug)]
 struct Response {
     name: String,
 }
 
+// -- define async main function
 #[tokio::main]
 async fn main() {
     // -- mockup user
@@ -23,19 +26,29 @@ async fn main() {
         email: "frr228@gmail.com".to_string(),
     };
 
+    // -- create a new Firebase instance with the Firebase database URL
     let firebase = Firebase::new("https://rust-firebase-default-rtdb.firebaseio.com/").unwrap();
 }
 
+// -- define async set_user function that sets user data in Firebase
 async fn set_user(firebase_client: &Firebase, user: &User) -> Response {
+    // -- get a reference to the "users" node in Firebase
     let firebase = firebase_client.at("users");
+    // -- set the user data in Firebase
     let _users = firebase.set::<User>(&user).await;
+    // -- return the response from Firebase as a Response struct
     return string_to_response(&_users.unwrap().data);
 }
 
+// -- define async get_users function that retrieves all users from Firebase
 async fn get_users(firebase_client: &Firebase, user: &User) -> HashMap<String, User> {
+    // -- get a reference to the "users" node in Firebase
     let firebase = firebase_client.at("users");
+    // -- get all user data from Firebase as a HashMap
     let users = firebase.get::<HashMap<String, User>>().await;
+    // -- print the retrieved user data
     println!("{:?}", users);
+    // -- return the retrieved user data as a HashMap
     return users.unwrap();
 }
 
